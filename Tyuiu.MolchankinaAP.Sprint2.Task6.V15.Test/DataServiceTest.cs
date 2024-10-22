52fdc8362d84ae5013b976a6b022035896034fe5
Ангелина Молчанкина <molchankinaap@outlook.com>
2024-10-22 11:52:09 +0500

    Task6

---
+++ Tyuiu.MolchankinaAP.Sprint2.Task6.V15.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+using Tyuiu.MolchankinaAP.Sprint2.Task6.V15.Lib;
+namespace Tyuiu.MolchankinaAP.Sprint2.Task6.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidFindDayName()
+        {
+            DataService service1 = new DataService();
+            int k = 251;
+            Assert.AreEqual("Суббота", service1.FindDayName(k));
+        }
+    }
+}
---
+++ Tyuiu.MolchankinaAP.Sprint2.Task6.V15/Program.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.MolchankinaAP.Sprint2.Task6.V15.Lib;
+namespace Tyuiu.MolchankinaAP.Sprint2.Task6.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService service1 = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите день для проверки= ");
+            int k = Convert.ToInt32(Console.ReadLine());
+            string res = service1.FindDayName(k);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("День недели заданного вами дня: " + res);
+            Console.ReadKey();
+        }
+    }
+}
